b44c8384b4fa872a4dd42f1b4e13756a6a53742d
Alberto <mail4alberto@gmail.com>
2015-09-20 23:36:29 -0400

    be able to pass object that'll be merged with the underlying react class, instead of functions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 const React = require('react');
+const Assign = require('lodash.assign');
 
 const DEFAULT_ASSIGN = function() {
     return {};
 };
 
-module.exports = function once(Component = null, assignPropsOnMount = DEFAULT_ASSIGN, cleanOnUnmount = DEFAULT_ASSIGN) {
+module.exports = function once(Component = null, onceSpec) {
 
     const ComponentDisplayName = Component && Component.displayName || 'null';
 
-    const OnceContainer = React.createClass({
+    const assignPropsOnMount = onceSpec.assignPropsOnMount ||  DEFAULT_ASSIGN;
+    const cleanOnUnmount = onceSpec.cleanOnUnmount || DEFAULT_ASSIGN;
+
+    const Overrides = {
 
         displayName: ComponentDisplayName + '.OnceContainer',
 
         getInitialState() {
             return {
-                cached: assignPropsOnMount.call(null, this.props) || {}
+                cached: assignPropsOnMount.call(null, this.props, this.context) || {}
             };
         },
 
         componentWillUnmount() {
-            cleanOnUnmount.call(null, this.state.cached);
+            cleanOnUnmount.call(null, this.state.cached, this.props, this.context);
         },
 
         render() {
             return (Component === null ? null : <Component {...this.props} {...this.state.cached} />);
         }
-    });
+    };
 
+    const OnceContainer = React.createClass(Assign({}, onceSpec, Overrides));
     return OnceContainer;
 };
